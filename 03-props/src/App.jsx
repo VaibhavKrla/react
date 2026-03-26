84ce158fe25b0aca1afb25e8f2e6c8264c044f8d
Vaibhav <vaibhavkrla@gmail.com>
2026-03-27 01:49:01 +0530

    folder updated

---
+++ 03-props/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="parent">
+      <div className="card">
+        <h1>Sarthak Sharma</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam,
+          dolores.
+        </p>
+        <button>View Profile</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
